faede4d00e5ecc8c10f4545a5f0bb96412965489
Michael Shuff <mjshuff23@gmail.com>
2020-12-28 18:15:48 -0500

    completed canSum, fib(memoized), gridTraveler

---
+++ howSum.js
@@ -0,0 +1,20 @@
+/*********************************** howSum *******************************************
+ *
+ *    - Write a function `howSum(targetSum, numbers)` that takes in a targetSum and an
+ *        array of numbers as arguments.
+ *
+ *    - The function should return an array containing any combinationg of elements that
+ *        add up to exactly the targetSum.
+ *      - If there are no combinations that add up to targetSum:
+ *          - Return null
+ *      - If there are multiple combinations possible:
+ *          - Return any combination that works
+ *
+ *    - Note that this is very similar to canSum, except that instead of returning
+ *        a boolean we return the pair of numbers in the array that sum to the targetSum
+ *
+ *    Arguments: targetSum: integer, numbers: array
+ *    Returns:   numbers: array... When summed === targetSum
+ *    Example 1: howSum(7, [5, 2]) => [5, 2]
+ *    Example 2: howSum(7, [4, 2]) => null
+ ***************************************************************************************/
